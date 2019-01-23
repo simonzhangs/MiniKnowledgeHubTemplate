004f73230774519b7e3dafb2da321966dc4dc894
DESKTOP-RQ9V16F\eagle <422964429@qq.com>
2019-01-23 13:21:14 +0800

    添加初始菜单项

--- example/index.js
+++ example/index.js
@@ -12,7 +12,7 @@ Page({
                 pages: [
                     { id: 'packages', name: '包' },
                     { id: 'imports', name: '导入' },
-                    { id: 'exported-namse', name: '导出名' },
+                    { id: 'exported-names', name: '导出名' },
                     { id: 'functions', name: '函数' },
                     { id: 'functions-continued', name: '函数（续）' },
                     { id: 'multiple-results', name: '多值返回' },
@@ -30,28 +30,97 @@ Page({
 
             },
             {
-                id: 'widget',
+                id: 'control',
                 name: '流程控制语句',
                 open: false,
-                pages: ['article', 'badge', 'flex', 'footer', 'gallery', 'grid', 'icons', 'loadmore', 'panel', 'preview', 'progress']
+                pages: [
+                    { id: 'go-for', name: 'for' },
+                    { id: 'for-continued', name: 'for（续）' },
+                    { id: 'for-is-gos-while', name: 'for是Go中的“while”' },
+                    { id: 'forever', name: '无限循环' },
+                    { id: 'go-if', name: 'if' },
+                    { id: 'if-with-a-short-statement', name: 'if的简短语句' },
+                    { id: 'if-and-else', name: 'if和else' },
+                    { id: 'go-switch', name: 'switch' },
+                    { id: 'switch-evaluation-order', name: 'switch的求值顺序' },
+                    { id: 'switch-with-no-condition', name: '没有条件的switch' },
+                    { id: 'go-defer', name: 'defer' },
+                    { id: 'defer-multi', name: 'defer栈' }
+                ]
             },
             {
-                id: 'feedback',
+                id: 'struct',
                 name: '结构体、切片和映射',
                 open: false,
-                pages: ['actionsheet', 'dialog', 'msg', 'picker', 'toast']
+                pages: [
+                    { id: 'pointers', name: '指针' },
+                    { id: 'structs', name: '结构体' },
+                    { id: 'struct-fields', name: '结构体字段' },
+                    { id: 'struct-pointers', name: '结构体指针' },
+                    { id: 'struct-literals', name: '结构体文法' },
+                    { id: 'go-array', name: '数组' },
+                    { id: 'slices', name: '切片' },
+                    { id: 'slices-pointers', name: '切片就像数组的引用' },
+                    { id: 'slice-literals', name: '切片文法' },
+                    { id: 'slice-bounds', name: '切片的默认行为' },
+                    { id: 'slice-len-cap', name: '切片的长度与容量' },
+                    { id: 'nil-slices', name: 'nil切片' },
+                    { id: 'making-slices', name: '用make创建切片' },
+                    { id: 'slices-of-slice', name: '切片的切片' },
+                    { id: 'append', name: '向切片追加元素' },
+                    { id: 'range', name: 'Range' },
+                    { id: 'range-continued', name: 'range（续）' },
+                    { id: 'go-maps', name: '映射' },
+                    { id: 'map-literals', name: '映射文法' },
+                    { id: 'map-literals-continued', name: '映射文法（续）' },
+                    { id: 'mutating-maps', name: '修改映射' },
+                    { id: 'function-values', name: '函数值' },
+                    { id: 'function-closures', name: '函数的闭包' }
+
+
+                ]
             },
             {
-                id: 'nav',
+                id: 'method',
                 name: '方法和接口',
                 open: false,
-                pages: ['navbar', 'tabbar']
+                pages: [
+                    { id: 'methods', name: '方法' },
+                    { id: 'methods-funcs', name: '方法即函数' },
+                    { id: 'methods-continued', name: '方法（续）' },
+                    { id: 'methods-pointers', name: '指针接收者' },
+                    { id: 'methods-pointers-explained', name: '指针与函数' },
+                    { id: 'indirection', name: '方法与指针重定向' },
+                    { id: 'indirection-values', name: '方法与指针重定向（续）' },
+                    { id: 'methods-with-pointer-receivers', name: '选择值或指针作为接收者' },
+                    { id: 'interfaces', name: '接口' },
+                    { id: 'interfaces-are-satisfied-implicitly', name: '接口与隐式实现' },
+                    { id: 'interface-values', name: '接口值' },
+                    { id: 'interface-values-with-nil', name: '底层值为nil的接口值' },
+                    { id: 'nil-interface-values', name: 'nil接口值' },
+                    { id: 'empty-interface', name: '空接口' },
+                    { id: 'type-assertions', name: '类型断言' },
+                    { id: 'type-switches', name: '类型选择' },
+                    { id: 'stringer', name: 'Stringer' },
+                    { id: 'errors', name: '错误' },
+                    { id: 'reader', name: 'Reader' },
+                    { id: 'go-images', name: '图像' }
+                ]
             },
             {
-                id: 'search',
+                id: 'concurrent',
                 name: '并发',
                 open: false,
-                pages: ['searchbar']
+                pages: [
+                    { id: 'goroutines', name: 'Go程' },
+                    { id: 'channels', name: '信道' },
+                    { id: 'buffered-channels', name: '带缓存的信道' },
+                    { id: 'range-and-close', name: 'range和close' },
+                    { id: 'select', name: 'select语句' },
+                    { id: 'default-selection', name: '默认选择' },
+                    { id: 'mutex-counter', name: 'sync.Mutex' },
+                    { id: 'exercise-web-crawler', name: 'Web爬虫' }
+                ]
             }
         ]
     },
