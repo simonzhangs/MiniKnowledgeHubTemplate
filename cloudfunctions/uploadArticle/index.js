1582c43285a2c21f65e38337c2a8514bc260719f
eagle <422964429@qq.com>
2021-08-22 00:55:43 +0800

    添加stars参数

--- cloudfunctions/uploadArticle/index.js
+++ cloudfunctions/uploadArticle/index.js
@@ -6,33 +6,35 @@ cloud.init({
 })
 
 const db = cloud.database()
-exports.main = async(event, context) => {
+exports.main = async (event, context) => {
   // 数据库结构，openid，title，浏览数量num，标签，创建时间，文件名（对应云存储），状态（是否显示）
   // 上传的参数，文件名，文件简介，标签，title
   var openid = event.openid
   var tag = event.tag
   var title = event.title
-  var fileid = event.fileid 
+  var fileid = event.fileid
   var desc = event.desc
-  try{
-    const result=await db.collection('artList').add({
+  var stars = parseInt(event.stars)
+  try {
+    const result = await db.collection('artList').add({
       // data 字段表示需新增的 JSON 数据
       data: {
         openid: openid,
         title: title,
-        desc:desc,
+        desc: desc,
         num: 0,
-        tag:tag,
-        fileid:fileid,
-        status:true,
-        createTime:db.serverDate()
+        tag: tag,
+        stars: stars,
+        fileid: fileid,
+        status: true,
+        createTime: db.serverDate()
       }
     })
     return result
 
-  }catch(e){
+  } catch (e) {
     console.error(e)
-    
+
   }
 
 }
