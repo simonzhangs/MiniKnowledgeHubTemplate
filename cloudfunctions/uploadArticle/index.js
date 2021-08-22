ed2062e0dab54f76334149e6d30b381adf03daab
eagle <422964429@qq.com>
2021-08-22 13:01:44 +0800

    添加title名称检查

--- cloudfunctions/uploadArticle/index.js
+++ cloudfunctions/uploadArticle/index.js
@@ -15,26 +15,64 @@ exports.main = async (event, context) => {
   var fileid = event.fileid
   var desc = event.desc
   var stars = parseInt(event.stars)
+  // 查看该记录是否存在，title 唯一
+
   try {
-    const result = await db.collection('artList').add({
-      // data 字段表示需新增的 JSON 数据
-      data: {
+    const res1 = await db.collection('artList').where({
         openid: openid,
         title: title,
-        desc: desc,
-        num: 0,
-        tag: tag,
-        stars: stars,
-        fileid: fileid,
-        status: true,
-        createTime: db.serverDate()
-      }
-    })
-    return result
+      })
+      .field({
+        _id: true,
+        fileid: true,
+      }).get()
+
+    if (res1.data.length != 0) {
+      // 删除原文件，然后更新信息。
+      console.log(res1.data)
+      const fileIDs = [res1.data[0].fileid]
+      const dfResp = await cloud.deleteFile({
+        fileList: fileIDs,
+      })
+      console.log(dfResp.fileList)
+      db.collection('artList').doc(res1.data[0]._id).update({
+        // data 传入需要局部更新的数据
+        data: {
+          desc: desc,
+          fileid: fileid,
+          tag: tag,
+          stars: stars,
+          createTime: db.serverDate()
+        },
+        success: function (res) {
+          console.log(res.data)
+        }
+      })
+
+    } else {
+      const res2 = await db.collection('artList').add({
+        // data 字段表示需新增的 JSON 数据
+        data: {
+          openid: openid,
+          title: title,
+          desc: desc,
+          num: 0,
+          tag: tag,
+          stars: stars,
+          fileid: fileid,
+          status: true,
+          createTime: db.serverDate()
+        }
+      })
+
+      console.log(res2._id)
+
+    }
+    return 'ok'
 
   } catch (e) {
-    console.error(e)
-
+    console.log(e)
+    return 'fail'
   }
 
 }
