71911a1c606af018a006492556cf702f4f64d77c
eagle zhang <422964429@qq.com>
2024-04-21 17:49:26 +0800

    添加激励视频广告

--- miniprogram/pages/my/index.js
+++ miniprogram/pages/my/index.js
@@ -1,14 +1,48 @@
 // pages/my/index.js
 const app = getApp();
 const utils = require('../../utils/utils.js')
-
+let videoAd = null
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-   
+
+  },
+
+  loadAd() {
+    if (wx.createRewardedVideoAd) {
+      videoAd = wx.createRewardedVideoAd({
+        adUnitId: 'adunit-2ce6db3cb1e45a86'
+      })
+      videoAd.onLoad(() => {})
+      videoAd.onError((err) => {
+        console.error('激励视频光告加载失败', err)
+      })
+      videoAd.onClose((res) => {
+        // 用户点击了【关闭广告】按钮
+        if (res && res.isEnded) {
+          // 正常播放结束，可以下发游戏奖励
+        } else {
+          // 播放中途退出，不下发游戏奖励
+        }
+      })
+    }
+  },
+
+  playAd() {
+    // 用户触发广告后，显示激励视频广告
+    if (videoAd) {
+      videoAd.show().catch(() => {
+        // 失败重试
+        videoAd.load()
+          .then(() => videoAd.show())
+          .catch(err => {
+            console.error('激励视频 广告显示失败', err)
+          })
+      })
+    }
   },
 
   /**
@@ -43,7 +77,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-
+    videoAd = null;
   },
 
   /**
