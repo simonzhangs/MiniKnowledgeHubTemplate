f3ae246508b97e430ead7a09ebe3b9158d050e1e
eagle <422964429@qq.com>
2024-04-22 21:39:45 +0800

    添加图片，优化我的页面

--- miniprogram/pages/my/index.js
+++ miniprogram/pages/my/index.js
@@ -1,46 +1,75 @@
 // pages/my/index.js
 const app = getApp();
 const utils = require('../../utils/utils.js')
-let videoAd = null
+let videoAd = null;
+let isLoadAd = false;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    points:50,
+    artnum:20,
+    views:10000,
+    lockviews:500,
+    sharepoints:20,
+    tmplpoints:10,
   },
 
   loadAd() {
+    const that = this;
     if (wx.createRewardedVideoAd) {
       videoAd = wx.createRewardedVideoAd({
         adUnitId: 'adunit-2ce6db3cb1e45a86'
       })
-      videoAd.onLoad(() => {})
+      videoAd.onLoad(() => {
+        that.isLoadAd = true;
+      })
       videoAd.onError((err) => {
         console.error('激励视频光告加载失败', err)
+        that.isLoadAd=false;
       })
       videoAd.onClose((res) => {
         // 用户点击了【关闭广告】按钮
         if (res && res.isEnded) {
           // 正常播放结束，可以下发游戏奖励
+          console.log('1')
         } else {
           // 播放中途退出，不下发游戏奖励
+          console.log('2')
         }
       })
     }
   },
 
   playAd() {
+    wx.showLoading({
+      title: '加载广告中',
+    })
+    if(!isLoadAd){
+      this.loadAd();
+      
+    }
     // 用户触发广告后，显示激励视频广告
     if (videoAd) {
+      wx.hideLoading()
       videoAd.show().catch(() => {
         // 失败重试
         videoAd.load()
-          .then(() => videoAd.show())
+          .then(() => {
+            
+            videoAd.show()})
           .catch(err => {
+            wx.showToast({
+              title: '请重试一次',
+            })
             console.error('激励视频 广告显示失败', err)
           })
+      })
+    }else{
+      wx.showToast({
+        title: '请重试一次',
       })
     }
   },
@@ -78,6 +107,7 @@ Page({
    */
   onUnload() {
     videoAd = null;
+    isLoadAd = false;
   },
 
   /**
